17b62bf9d4e0269ca077a1acc4bbb7359632a29c
Felice Pollano <felice.pollano@ors.it>
2020-06-01 14:29:19 +0200

    Updates

---
+++ precache-manifest.5ebaa400fa47b5a0d034bd030bbd6b0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a0bc1b90642cbfbbaea4c215f57f40",
+    "url": "/leo-app/index.html"
+  },
+  {
+    "revision": "3a8c79a8921ff82e6804",
+    "url": "/leo-app/static/css/main.e069c1f0.chunk.css"
+  },
+  {
+    "revision": "4df51dffbd7e3480fd97",
+    "url": "/leo-app/static/js/2.2d7f0268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/leo-app/static/js/2.2d7f0268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8c79a8921ff82e6804",
+    "url": "/leo-app/static/js/main.38591acb.chunk.js"
+  },
+  {
+    "revision": "9bc6405826c95386683b",
+    "url": "/leo-app/static/js/runtime-main.a44b684f.js"
+  }
+]);
